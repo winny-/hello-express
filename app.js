cf9357c7983cf66b9d7b64fe8be3362d918ec98a
winny <winston@ml1.net>
2013-08-05 15:31:21 -0500

    pg

--- app.js
+++ app.js
@@ -1,39 +1,44 @@
 var express = require('express');
 var mu = require('mu2');
-var sqlite3 = require('sqlite3');
+var pg = require('pg');
 
 var app = express();
-var db = new sqlite3.Database('data.sqlite3');
-db.serialize();
-db.exec('CREATE TABLE IF NOT EXISTS counter (address TEXT)');
+var client = new pg.Client({database: process.env.DATABASE_URL});
 
+function handleError(err){
+  if (err) console.log(err);
+}
 
-app.get('/', function(req, res){
-  db.serialize(function (){
-    db.run('INSERT INTO counter (address) VALUES ($address)', {$address: req.ip});
+pg.connect(process.env.DATABASE_URL, function(err, client, done){
 
-    db.get('SELECT COUNT(*) AS c FROM counter', function (err, row){
-      visits = row.c;
-      var body = '';
+  client.query('CREATE TABLE IF NOT EXISTS counter (address TEXT)', function(err, result){
+    handleError(err);
 
-      var stream = mu.compileAndRender('hello.html', {title: 'Hello, world!', ip: req.ip, visits: visits });
+    app.get('/', function(req, res){
+      client.query('INSERT INTO counter (address) VALUES ($1)', req.ip, function(err, result){
+        client.query('SELECT COUNT(*) AS c FROM counter', null, function (err, result){
+          visits = result.rows[0].count;
+          var body = '';
 
-      res.setHeader('Content-Type', 'text/html');
+          var stream = mu.compileAndRender('hello.html', {title: 'Hello, world!', ip: req.ip, visits: visits });
 
-      stream.on('data', function (data){
-        body += data.toString();
+          res.setHeader('Content-Type', 'text/html');
+
+          stream.on('data', function (data){
+            body += data.toString();
+          });
+
+          stream.on('end', function (){
+            res.setHeader('Content-Length', body.length);
+            res.end(body);
+          });
+
+        });
       });
-
-      stream.on('end', function (){
-        res.setHeader('Content-Length', body.length);
-        res.end(body);
-      });
-
     });
   });
+  var port = 3000;
+  app.listen(port);
+  console.log('Listening on port ' + port + '.');
+
 });
-
-
-var port = 3000;
-app.listen(port);
-console.log('Listening on port ' + port + '.');
