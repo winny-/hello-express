349aee3c0c9a8416d925c0e6847455895e9228f7
winny <winston@ml1.net>
2013-08-05 16:22:39 -0500

    para

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ pg.connect(process.env.DATABASE_URL, function(err, client, done){
     handleError(err);
 
     app.get('/', function(req, res){
-      client.query('INSERT INTO counter (address) VALUES ($1)', req.ip, function(err, result){
+      client.query({text: 'INSERT INTO counter (address) VALUES ($1)', values: [req.ip]}, function(err, result){
         handleError(err);
         client.query('SELECT COUNT(*) AS c FROM counter', null, function (err, result){
           handleError(err);
